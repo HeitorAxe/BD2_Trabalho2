a7eb63ad41043d5c7f7042b1cf820fac8a66ad5f
Heitor Machado <90213050+HeitorAxe@users.noreply.github.com>
2023-05-28 21:35:25 -0300

    Alterações finais

---
+++ README.md
@@ -0,0 +1,14 @@
+# Trabalho Prático - LOG - BDII
+**Alunos: Heitor Machado Pedro Spegiorin**
+
+**Repositório Usado Como Base: https://github.com/letyrsl/trabalho-log-bd2**
+
+O objetivo desse trabalho de Banco de Dados II é implementar o mecanismo de log Redo e Undo sem checkpoint usando o SGBD.
+
+## Para rodar
+- É necessário ter instaladas as seguintes dependências:
+  - Python 3.x
+  - Postgres 14.x
+- É necessária a instalação do pacote psycopg2: **pip install psycopg2**
+- É necessária a criação da database db_log: **CREATE DATABASE db_log;**
+- Tendo seguido todos os passos anteriores, é possível rodar o projeto através do comando: **python main.py**
---
+++ main.py
@@ -0,0 +1,41 @@
+# Pacote adaptador do banco de dados
+import psycopg2
+
+# Scrips desenvolvidos para o projeto
+from scripts.db_config import db_config
+from scripts.load_database import load_database
+from scripts.log_redo import recover
+
+def main():
+  conn = None
+  try:
+    # conecta com banco de dados e cria cursor
+    params = db_config()
+    conn = psycopg2.connect(**params)
+    cursor = conn.cursor()
+
+    #creat
+
+	  # carrega o banco com dados do arquivo
+    load_database(cursor)
+
+    # recuperar log REDO
+    recover(cursor)
+
+    #commitando mudanças
+    #desnecessário
+    conn.commit()
+
+	  # fecha conexão com banco
+    cursor.close()
+
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+  finally:
+    if conn is not None:
+      conn.close()
+
+
+if __name__ == '__main__':
+  main()
